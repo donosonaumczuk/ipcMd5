2f7acd04b334daa4694bbabde5e38b263c28ea39
AgustinIzaguirre <Agustinizag1@gmail.com>
2018-04-02 17:18:31 -0700

    changes before merge

--- src/slaveProcess.c
+++ src/slaveProcess.c
@@ -1,6 +1,16 @@
 #include <slaveProcess.h>
 
 int main() {
+    sem_unlink("/requestSemaphore");
+    sem_t *requestSem = sem_open("/requestSemaphore", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR, 1);
+    sem_unlink("/md5Semaphore");
+    sem_t *md5Sem = sem_open("/md5Semaphore", O_CREAT | O_RDWR, S_IRUSR | S_IWUSR, 1);
+
+
+    sem_t *requestSem = sem_open("/requestSemaphore", O_WRONLY);
+    sem_t *md5Sem = sem_open("/md5Semaphore", O_WRONLY);
+
+    
     char fifoPaths[MAX_LONG_DIGITS];
     int fdPaths, fdRequest, fdMd5, number;
     fdRequest = open(AVAILABLE_SLAVES_QUEUE, O_WRONLY);
@@ -11,7 +21,7 @@ int main() {
     if(fdMd5 == ERROR_STATE) {
         error("");
     }
-    createFilePathFifo(fifoPaths, fdRequest);
+    createFilePathFifo(fifoPaths, fdRequest, requestSem);
     fdPaths = open(fifoPaths, O_RDONLY);
     if(fdPaths == ERROR_STATE) {
         error("");
@@ -21,9 +31,11 @@ int main() {
         number = getNumberOfFilePaths(fdPaths);
         if(number) {
             hashFilesOfGivenPaths(number, fdPaths, fdMd5);
+            sem_wait(requestSem);
             if(write(fdRequest, fifoPaths, strlen(fifoPaths)) == ERROR_STATE) {
                 error("");
             }
+            sem_post(requestSem);
         }
     } while(number);
 
@@ -33,12 +45,14 @@ int main() {
     return 0;
 }
 
-void createFilePathFifo(char *name, int fdRequest) {
+void createFilePathFifo(char *name, int fdRequest, sem_t *requestSem) {
     sprintf(name, "%d", getpid());
     if(mkfifo(name, S_IRUSR | S_IWUSR) == ERROR_STATE) {
         error("");
     }
+    sem_wait(requestSem);
     if(write(fdRequest, name, strlen(name) + 1) == ERROR_STATE) {
         error("");
     }
+    sem_post(requestSem);
 }
